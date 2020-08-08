ebb05101969fe6692bd230960967e44ef2cc443c
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2020-08-08 16:33:42 -0300

    Remove book files

--- infraestructure/tables.js
+++ infraestructure/tables.js
@@ -1,23 +1,9 @@
 class Tables {
     init (connection) {
-        //this.connection = connection;
         console.log("tables creation was called");
 
         this.connection = connection;
-        this.createBooksTable();
         this.createUsersTable();
-    }
-
-    createBooksTable() {
-        const sql = 'CREATE TABLE IF NOT EXISTS books (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, description text, price int NOT NULL, PRIMARY KEY(id))';
-
-        this.connection.query(sql, (error => {
-            if(error) {
-                console.log(error);
-            } else {
-                console.log("Books table created successfully")
-            }
-        }));
     }
 
     createUsersTable() {
