f5815b050033e3229cdd432f1fa46503b78709f1
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2020-08-08 16:08:01 -0300

    Remove comment

--- infraestructure/tables.js
+++ infraestructure/tables.js
@@ -8,9 +8,6 @@ class Tables {
     }
 
     createBooksTable() {
-        //update SQL
-        /*const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'*/
-        
         const sql = 'CREATE TABLE IF NOT EXISTS books (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, description text, price int NOT NULL, PRIMARY KEY(id))';
 
         this.connection.query(sql, (error => {
